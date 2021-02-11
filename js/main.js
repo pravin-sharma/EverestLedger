d011272ba668a8065196e031d1b7f19b1d4f0df1
Pravin Sharma <Pravin.Sharma@lntinfotech.com>
2021-02-11 16:01:46 +0530

    Added responsiveness for mobile and widescreen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+// Google Maps
 function initMap() {
   // Your location
   const loc = { lat: 42.361145, lng: -71.057083 };
@@ -9,3 +11,23 @@ function initMap() {
   // The marker, positioned at location
   const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+// Smooth Scroll - JQuery
+  $('#navbar a, #showcase a').on('click',function(event){
+    if(this.hash != ''){
+      //console.log(this);
+      event.preventDefault();
+  
+      const hash = this.hash;
+      $('html,body').animate({scrollTop: $(hash).offset().top - 100},800)
+    }
+  })
+ 
+// Reduce Opacity on scroll - JS
+window.addEventListener('scroll',function(){
+  if(window.scrollY > 150 && window.innerWidth > 344 ){
+    document.querySelector('#navbar').style.opacity = 0.9;
+  }else{
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+})
